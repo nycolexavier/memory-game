03f0539e9334ba9aaf671e06076a2e619ba2b791
Nycole <nycole.xavierr@gmail.com>
2021-08-26 07:39:37 -0300

    card option

---
+++ game.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // card option
+    const cardArray = [{
+        name: 'blue',
+        img: 'assets/blue.png'
+    },
+    {
+        name: 'blue',
+        img: 'assets/blue.png'
+    },
+    {
+        name: 'gray',
+        img: 'assets/gray.png'
+    },
+    {
+        name: 'gray',
+        img: 'assets/gray.png'
+    },
+    {
+        name: 'green',
+        img: 'assets/green.png'
+    },
+    {
+        name: 'green',
+        img: 'assets/green.png'
+    },
+    {
+        name: 'purple',
+        img: 'assets/purple.png'
+    },
+    {
+        name: 'purple',
+        img: 'assets/purple.png'
+    },
+    {
+        name: 'red',
+        img: 'assets/red.png'
+    },
+    {
+        name: 'red',
+        img: 'assets/red.png'
+    },
+    {
+        name: 'yellow',
+        img: 'assets/yellow.png'
+    },
+    {
+        name: 'yellow',
+        img: 'assets/yellow.png'
+    }
+]
+})
