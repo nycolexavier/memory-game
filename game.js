27f49ecc76a8436e1a8a248ba7ec496aba126708
Nycole <nycole.xavierr@gmail.com>
2021-08-26 08:19:32 -0300

    check for matches and flips

--- game.js
+++ game.js
@@ -52,19 +52,45 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
 
     const grid = document.querySelector('grid')
+    var cardsChosen = []
+    var cardsChosenId = []
 
     // create your board
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
             var card = document.createElement('img')
-            card.setAttribute('src', 'images/blank.png')
+            card.setAttribute('src', 'assets/blank.png')
             card.setAttribute('data-id', i)
-            // card.addEventListener('click', flipcard)
+            // card.addEventListener('click', flipCard)
             grid.appendChild(card)
         }
     }
 
+    // check for matches
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match')
+            cards[optionOneId].setAttribute('src', 'assets/white.png')
+            cards[optionTwoId].setAttribute('src', 'assets/white.png')
+            cards
+        }
+    }
 
+    // flips
+    function flipCard() {
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
+    }
+
+    createBoard();
 
 
 })
